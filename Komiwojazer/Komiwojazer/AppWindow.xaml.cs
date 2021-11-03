9cd61573b718c5fd105e01e6bf64b2a4f06f3920
AleksanderMaciejewski <olekmaciejewski9@gmail.com>
2021-11-03 14:25:53 +0100

    Buttons logic

--- Komiwojazer/Komiwojazer/AppWindow.xaml.cs
+++ Komiwojazer/Komiwojazer/AppWindow.xaml.cs
@@ -18,6 +18,11 @@ namespace Komiwojazer
     public partial class AppWindow : Window
     {
         private int flag { get; set; } = -1;
+
+        private List<Ellipse> points { get; set; } = new List<Ellipse>();
+        private Ellipse startingPoint { get; set; } = null;
+        private int licznik { get; set; } = 0;
+
         public AppWindow()
         {
             InitializeComponent();
@@ -30,31 +35,43 @@ namespace Komiwojazer
 
         private void CanvasImage_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
             var coord = e.GetPosition(this.CanvasImage);
-            //MessageBox.Show($"X:{coord.X}, Y:{coord.Y}");
-            var punkt = new Ellipse();
-            if (coord.X < 764 && coord.Y < 577
-                && coord.X>2 && coord.Y>3)
+            if (coord.X < 764 && coord.Y < 577 && coord.X > 2 && coord.Y > 3)
             {
-                punkt.Width = 10;
-                punkt.Height = 10;
-                if (flag == 1) punkt.Fill = Brushes.Red;
-                else if (flag == 2) punkt.Fill = Brushes.Blue;
-                Canvas.SetLeft(punkt, coord.X - 5);
-                Canvas.SetTop(punkt, coord.Y - 5);
-                CanvasImage.Children.Add(punkt);
+                if (flag == 1)
+                {
+                    if (startingPoint == null)
+                    {
+                        startingPoint = new Ellipse();
+                        startingPoint.Width = 10;
+                        startingPoint.Height = 10;
+                        startingPoint.Fill = Brushes.Red;
+                        CanvasImage.Children.Add(startingPoint);
+                    }
+                    Canvas.SetLeft(startingPoint, coord.X - 5);
+                    Canvas.SetTop(startingPoint, coord.Y - 5);
+                }
+                else if (flag == 2)
+                {
+                    points.Add(new Ellipse());
+                    points[licznik].Width = 10;
+                    points[licznik].Height = 10;
+                    points[licznik].Fill = Brushes.Blue;
+                    Canvas.SetLeft(points[licznik], coord.X - 5);
+                    Canvas.SetTop(points[licznik], coord.Y - 5);
+                    CanvasImage.Children.Add(points[licznik++]);
+                }
             }
         }
 
         private void CanvasImage_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            
+
         }
 
         private void CanvasImage_MouseMove(object sender, MouseEventArgs e)
         {
-            
+
         }
 
         private void CanvasImage_PreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -70,6 +87,23 @@ namespace Komiwojazer
         private void endPointsButton_Click(object sender, RoutedEventArgs e)
         {
             flag = 2;
+        }
+
+        private void removePointsButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var point in points)
+            {
+                CanvasImage.Children.Remove(point);
+            }
+            licznik = 0;
+        }
+
+        private void startAlgorithmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (startingPoint == null || licznik < 1)
+            {
+                MessageBox.Show("Nie wybrano punktów");
+            }
         }
     }
 }
