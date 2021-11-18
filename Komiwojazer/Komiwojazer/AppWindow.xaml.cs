a08844cedd16337e4ac3850b3f28205cc5375171
Michal Chudzik <80217047+Michal-Chudzik@users.noreply.github.com>
2021-11-18 01:25:19 +0100

    to jest dramat

--- Komiwojazer/Komiwojazer/AppWindow.xaml.cs
+++ Komiwojazer/Komiwojazer/AppWindow.xaml.cs
@@ -18,9 +18,11 @@ namespace Komiwojazer
     public partial class AppWindow : Window
     {
         private int _flag = -1;
-
-        private IList<Ellipse> _points = new List<Ellipse>();
-        private Ellipse _startingPoint = null;
+        private IList<Line> _lines = new List<Line>();
+        private int _linesCounter = 0;
+        //private IList<Ellipse> _points = new List<Ellipse>();
+        private Points _startingPoint = null;
+        private IList<Points> _points = new List<Points>();
         private int _counter = 0;
 
         public AppWindow()
@@ -42,24 +44,37 @@ namespace Komiwojazer
                 {
                     if (_startingPoint == null)
                     {
-                        _startingPoint = new Ellipse();
-                        _startingPoint.Width = 10;
-                        _startingPoint.Height = 10;
-                        _startingPoint.Fill = Brushes.Red;
-                        CanvasImage.Children.Add(_startingPoint);
+                        _startingPoint = new Points();
+                        _startingPoint.point = new Ellipse();
+                        _startingPoint.point.Width = 10;
+                        _startingPoint.point.Height = 10;
+                        _startingPoint.point.Fill = Brushes.Red;
+                        CanvasImage.Children.Add(_startingPoint.point);
                     }
-                    Canvas.SetLeft(_startingPoint, coord.X - 5);
-                    Canvas.SetTop(_startingPoint, coord.Y - 5);
+                    Canvas.SetLeft(_startingPoint.point, coord.X - 5);
+                    Canvas.SetTop(_startingPoint.point, coord.Y - 5);
+                    _startingPoint.Coor = coord;
+                    int col = 0, row = 0;
+                    fun(ref col, ref row);
+                    _startingPoint.Column = col;
+                    _startingPoint.Row = row;
                 }
                 else if (_flag == 2)
                 {
-                    _points.Add(new Ellipse());
-                    _points[_counter].Width = 10;
-                    _points[_counter].Height = 10;
-                    _points[_counter].Fill = Brushes.Blue;
-                    Canvas.SetLeft(_points[_counter], coord.X - 5);
-                    Canvas.SetTop(_points[_counter], coord.Y - 5);
-                    CanvasImage.Children.Add(_points[_counter++]);
+                    _points.Add(new Points());
+                    _points[_counter].point = new Ellipse();
+                    _points[_counter].point.Width = 10;
+                    _points[_counter].point.Height = 10;
+                    _points[_counter].point.Fill = Brushes.Blue;
+                    Canvas.SetLeft(_points[_counter].point, coord.X - 5);
+                    Canvas.SetTop(_points[_counter].point, coord.Y - 5);
+                    CanvasImage.Children.Add(_points[_counter].point);
+                    _points[_counter].Coor = coord;
+                    int col = 0, row = 0;
+                    fun(ref col, ref row);
+                    _points[_counter].Column = col;
+                    _points[_counter].Row = row;
+                    _counter++;
                 }
             }
         }
@@ -93,17 +108,225 @@ namespace Komiwojazer
         {
             foreach (var point in _points)
             {
-                CanvasImage.Children.Remove(point);
+                CanvasImage.Children.Remove(point.point);
+
             }
             _counter = 0;
+        }
+
+        private void lineVert(int x, int y)
+        {
+
+            _lines.Add(new Line
+            {
+                Stroke = System.Windows.Media.Brushes.Red,
+                Fill = System.Windows.Media.Brushes.Red,
+                X1 = 0,
+                X2 = 500,
+                StrokeThickness = 4,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Bottom
+
+            });
+            imageGrid.Children.Add(_lines[_linesCounter]);
+            Grid.SetRow(_lines[_linesCounter], x);
+            Grid.SetColumn(_lines[_linesCounter++], y);
+        }
+        private void lineHorDo(int x, int y)
+        {
+            _lines.Add(new Line
+            {
+                Stroke = System.Windows.Media.Brushes.Red,
+                Fill = System.Windows.Media.Brushes.Red,
+                Y1 = 0,
+                Y2 = 500,
+                StrokeThickness = 4,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom
+
+            });
+            imageGrid.Children.Add(_lines[_linesCounter]);
+            Grid.SetRow(_lines[_linesCounter], x);
+            Grid.SetColumn(_lines[_linesCounter++], y);
+        }
+        private void lineHorUp(int x, int y)
+        {
+            _lines.Add(new Line
+            {
+                Stroke = System.Windows.Media.Brushes.Red,
+                Fill = System.Windows.Media.Brushes.Red,
+                Y1 = 0,
+                Y2 = 500,
+                StrokeThickness = 4,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Bottom
+
+            });
+            imageGrid.Children.Add(_lines[_linesCounter]);
+            Grid.SetRow(_lines[_linesCounter], x);
+            Grid.SetColumn(_lines[_linesCounter++], y);
+        }
+
+        void fun(ref int col, ref int row)
+        {
+            var point = Mouse.GetPosition(imageGrid);
+
+            
+            double accumulatedHeight = 0.0;
+            double accumulatedWidth = 0.0;
+
+            // calc row mouse was over
+            foreach (var rowDefinition in imageGrid.RowDefinitions)
+            {
+                accumulatedHeight += rowDefinition.ActualHeight;
+                if (accumulatedHeight >= point.Y)
+                    break;
+                row++;
+            }
+
+            // calc col mouse was over
+            foreach (var columnDefinition in imageGrid.ColumnDefinitions)
+            {
+                accumulatedWidth += columnDefinition.ActualWidth;
+                if (accumulatedWidth >= point.X)
+                    break;
+                col++;
+            }
+            //MessageBox.Show($"row{row}  col{col}");
         }
 
         private void startAlgorithmButton_Click(object sender, RoutedEventArgs e)
         {
             if (_startingPoint == null || _counter < 1)
             {
-                MessageBox.Show("Nie wybrano punktów");
+                //MessageBox.Show("Nie wybrano punktów");
             }
+            int row = _startingPoint.Row, col = _startingPoint.Column;
+            for (int i = 0; i < _counter; i++)
+            {
+                while (row != _points[i].Row)
+                {
+                    if (row < _points[i].Row)
+                    {
+                        lineHorDo(row, col);
+                        row++;
+                        if (row == _points[i].Row) lineHorDo(row, col);
+                    }
+                    else if (row > _points[i].Row)
+                    {
+                        lineHorUp(row, col);
+                        row--;
+                    }
+                }
+
+                //lineHorDo(row, col);
+                //row--;
+                //col++;
+                while (col != _points[i].Column)
+                {
+                    if (col < _points[i].Column)
+                    {
+
+                        col++;
+                        lineVert(row, col);
+                    }
+                    else if (col > _points[i].Column)
+                    {
+                        lineVert(row, col);
+                        col--;
+                    }
+                }
+                //MessageBox.Show($"row{row}  col{col}");
+                lineVert(row, col);
+                row = _points[i].Row;
+                col = _points[i].Column;
+            }
+            //while (row != _startingPoint.Row)
+            //{
+            //    if (row < _startingPoint.Row)
+            //    {
+            //        lineHorDo(row, col);
+            //        row++;
+            //        if (row == _startingPoint.Row) lineHorDo(row, col);
+            //    }
+            //    else if (row > _startingPoint.Row)
+            //    {
+            //        lineHorUp(row, col);
+            //        row--;
+            //    }
+            //}
+
+            //lineHorDo(row, col);
+            //row--;
+            //col++;
+            //while (col != _startingPoint.Column)
+            //{
+            //    if (col < _startingPoint.Column)
+            //    {
+
+            //        col++;
+            //        lineVert(row, col);
+            //    }
+            //    else if (col > _startingPoint.Column)
+            //    {
+            //        lineVert(row, col);
+            //        col--;
+            //    }
+            //}
+
+
+
+
+        }
+
+        private void imageGrid_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //var point = Mouse.GetPosition(imageGrid);
+
+            //int row = 0;
+            //int col = 0;
+            //double accumulatedHeight = 0.0;
+            //double accumulatedWidth = 0.0;
+
+            //// calc row mouse was over
+            //foreach (var rowDefinition in imageGrid.RowDefinitions)
+            //{
+            //    accumulatedHeight += rowDefinition.ActualHeight;
+            //    if (accumulatedHeight >= point.Y)
+            //        break;
+            //    row++;
+            //}
+
+            //// calc col mouse was over
+            //foreach (var columnDefinition in imageGrid.ColumnDefinitions)
+            //{
+            //    accumulatedWidth += columnDefinition.ActualWidth;
+            //    if (accumulatedWidth >= point.X)
+            //        break;
+            //    col++;
+            //}
+            //MessageBox.Show($"row{row}  col{col}");
+            int col = 0;
+            int row = 0;
+            fun(ref col, ref row);
+
+            //lineVert(row, col++);
+            //lineVert(row++, col);
+            //lineHor(row++, col);
+            //lineHor(row, col++);
+            //lineVert(row, col++);
+            //lineVert(row, col++);
+
+        }
+
+        private void removeRoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var line in _lines)
+            {
+                imageGrid.Children.Remove(line);
+
+            }
+            //_linesCounter = 0;
         }
     }
 }
---
+++ Komiwojazer/Komiwojazer/Points.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows;
+
+
+namespace Komiwojazer
+{
+    class Points
+    {
+        public Ellipse point = null;
+        public Point Coor;
+        public int Column = -1;
+        public int Row = -1;
+        public Points()
+        {
+
+        }
+    }
+}
